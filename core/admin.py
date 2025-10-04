a01431c77fb0da48c2d9ac77fe621ba8ef896d06
kaustavray21 <kaustav.raysinha@gmail.com>
2025-10-04 16:17:47 +0530

    configured the admin in views

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course, Video, Enrollment, Note
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Video)
+admin.site.register(Enrollment)
+admin.site.register(Note)
+
